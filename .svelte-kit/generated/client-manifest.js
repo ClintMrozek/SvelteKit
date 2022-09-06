87b4aa9069782b47b971a5486283b6b2c128c501
ClintMrozek <clint2002mrozek@gmail.com>
2022-09-06 14:16:43 +0200

    Debug finish

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -1,19 +1,19 @@
 export { matchers } from './client-matchers.js';
 
-export const nodes = [
-	() => import('./nodes/0'),
+export const nodes = [() => import('./nodes/0'),
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
 	() => import('./nodes/5'),
-	() => import('./nodes/6')
-];
+	() => import('./nodes/6')];
+
+export const server_loads = [];
 
 export const dictionary = {
-	"": [[1], [0], 2],
-	"Blog": [[1], [0], 3],
-	"Clock": [[1], [0], 4],
-	"Counter": [[1], [0], 5],
-	"ToDo": [[1], [0], 6]
+	"": [2],
+	"Blog": [3],
+	"Clock": [4],
+	"Counter": [5],
+	"ToDo": [6]
 };
